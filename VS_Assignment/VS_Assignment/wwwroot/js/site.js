aefc48b2997aeff46c1be5c932fa7dcc5bc75124
SinisterGold <128642241+SinisterGold@users.noreply.github.com>
2025-02-14 03:54:43 -0700

    VS Assignment

---
+++ VS_Assignment/VS_Assignment/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var i = 0;
+var txt = 'Welcome to my webpage written using Visual Studio';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("type-writer").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
